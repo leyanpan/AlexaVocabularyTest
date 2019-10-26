07a8afe1ce8462788c196c3d7515549266fe86e7
CPA872 <cathay872@163.com>
2019-10-26 10:30:36 -0400

    Prototype of backing class VocabTester

---
+++ VocabTester/src/VocabTester.java
@@ -0,0 +1,51 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class VocabTester {
+    private HashMap<String, String> EnglishToSpanish = new HashMap<>();
+    private HashMap<String, String> SpanishToEnglish = new HashMap<>();
+
+
+    public VocabTester(ArrayList<String> English, ArrayList<String> Spanish) {
+        for (int i = 0; i < English.size(); i++) {
+            this.EnglishToSpanish.put(English.get(i), Spanish.get(i));
+            this.SpanishToEnglish.put(Spanish.get(i), English.get(i));
+        }
+    }
+
+    public HashMap<String, String> getEnglishToSpanish() {
+        return EnglishToSpanish;
+    }
+
+    public HashMap<String, String> getSpanishToEnglish() {
+        return SpanishToEnglish;
+    }
+
+    public String getEnglish(String Spanish) {
+        return this.SpanishToEnglish.get(Spanish);
+    }
+
+    public String getSpanish(String English) {
+        return this.EnglishToSpanish.get(English);
+    }
+
+    public static void main(String[] args) {
+        String[] EnglishWords = {"hello", "he", "she", "I", "you"};
+        String[] SpanishWords = {"hola", "él", "ella", "yo", "tú"};
+        ArrayList<String> English = new ArrayList<>();
+        ArrayList<String> Spanish = new ArrayList<>();
+
+        for (int i = 0; i < EnglishWords.length; i++) {
+            English.add(EnglishWords[i]);
+            Spanish.add(SpanishWords[i]);
+        }
+
+        VocabTester Tester1 = new VocabTester(English, Spanish);
+
+        System.out.println(Tester1.getSpanish("he"));
+        System.out.println(Tester1.getEnglish("ella"));
+
+    }
+}
