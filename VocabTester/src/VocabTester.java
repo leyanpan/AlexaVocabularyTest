3a8a6fe259b02b2ba277d4b03a01b68bdcfb3d17
CPA872 <cathay872@163.com>
2019-10-26 10:54:21 -0400

    modify

--- VocabTester/src/VocabTester.java
+++ VocabTester/src/VocabTester.java
@@ -1,25 +1,25 @@
 import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 public class VocabTester {
-    private HashMap<String, String> EnglishToSpanish = new HashMap<>();
-    private HashMap<String, String> SpanishToEnglish = new HashMap<>();
+    private Map<String, String> EnglishToSpanish = new HashMap<>();
+    private Map<String, String> SpanishToEnglish = new HashMap<>();
 
 
-    public VocabTester(ArrayList<String> English, ArrayList<String> Spanish) {
+    public VocabTester(List<String> English, List<String> Spanish) {
         for (int i = 0; i < English.size(); i++) {
             this.EnglishToSpanish.put(English.get(i), Spanish.get(i));
             this.SpanishToEnglish.put(Spanish.get(i), English.get(i));
         }
     }
 
-    public HashMap<String, String> getEnglishToSpanish() {
+    public Map<String, String> getEnglishToSpanish() {
         return EnglishToSpanish;
     }
 
-    public HashMap<String, String> getSpanishToEnglish() {
+    public Map<String, String> getSpanishToEnglish() {
         return SpanishToEnglish;
     }
 
@@ -34,8 +34,8 @@ public class VocabTester {
     public static void main(String[] args) {
         String[] EnglishWords = {"hello", "he", "she", "I", "you"};
         String[] SpanishWords = {"hola", "él", "ella", "yo", "tú"};
-        ArrayList<String> English = new ArrayList<>();
-        ArrayList<String> Spanish = new ArrayList<>();
+        List<String> English = new ArrayList<>();
+        List<String> Spanish = new ArrayList<>();
 
         for (int i = 0; i < EnglishWords.length; i++) {
             English.add(EnglishWords[i]);
